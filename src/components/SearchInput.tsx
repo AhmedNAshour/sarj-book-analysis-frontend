2bd0d6650105bb1b22b407d6d6064e2d449c6853
Ahmed Ashour <ahmed.nasr.ashour@gmail.com>
2025-04-01 21:13:06 +0200

    Fix typescript error

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Loader2, Search, Book, Zap, SearchCheck, RefreshCw } from "lucide-react";
